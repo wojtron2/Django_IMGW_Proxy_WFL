b7d3ac2c6c4b83a501d672888b9530a8a9c96d39
wojtron <67229366+wojtron2@users.noreply.github.com>
2025-09-12 22:34:32 +0200

    routing

--- imgw_proxy1/imgw_proxy_app/views.py
+++ imgw_proxy1/imgw_proxy_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Warning
 
-# Create your views here.
+@api_view(["GET"])
+def warnings_for_point(request):
+    items = Warning.objects.filter(is_active=True).order_by("valid_to")
+    data = [w.raw for w in items]
+    return Response({"count": len(data), "warnings": data})
