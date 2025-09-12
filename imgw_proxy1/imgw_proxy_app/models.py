ec70cea301093d5a5a7fda1903fc444c332c4051
wojtron <67229366+wojtron2@users.noreply.github.com>
2025-09-12 21:26:48 +0200

    models.py

--- imgw_proxy1/imgw_proxy_app/models.py
+++ imgw_proxy1/imgw_proxy_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Warning(models.Model):
+    uid = models.CharField(max_length=128, unique=True, db_index=True)
+    teryt_powiat = models.CharField(max_length=6, null=True, blank=True, db_index=True)
+    teryt_woj = models.CharField(max_length=2, null=True, blank=True, db_index=True)
+    valid_from = models.DateTimeField(null=True, blank=True, db_index=True)
+    valid_to = models.DateTimeField(null=True, blank=True, db_index=True)
+    is_active = models.BooleanField(default=True, db_index=True)
+    fetched_at = models.DateTimeField(auto_now=True)
+    raw = models.JSONField()
+
+    class Meta:
+        indexes = [models.Index(fields=["is_active", "valid_to"])]
