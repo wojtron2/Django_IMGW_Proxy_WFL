e79fae7aacbb7434962a7e650c0f2ce57ce3c463
wojtron <67229366+wojtron2@users.noreply.github.com>
2025-09-13 00:15:43 +0200

    set json output for endpoints

--- imgw_proxy1/core/settings.py
+++ imgw_proxy1/core/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+    ],
+}
