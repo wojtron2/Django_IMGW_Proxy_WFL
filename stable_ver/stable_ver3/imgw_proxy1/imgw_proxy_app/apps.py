ae58a583fc1d874e40597322c50ac192d4876811
wojtron <67229366+wojtron2@users.noreply.github.com>
2025-09-14 13:03:37 +0200

    localcopy3

---
+++ stable_ver/stable_ver3/imgw_proxy1/imgw_proxy_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ImgwProxyAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'imgw_proxy_app'
---
+++ stable_ver/stable_ver3/imgw_proxy1/imgw_proxy_app/views.py
@@ -0,0 +1,15 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Warning
+
+@api_view(["GET"])
+def warnings_for_point(request):
+    items = Warning.objects.filter(is_active=True).order_by("valid_to")
+    data = [w.raw for w in items]
+    return Response({"count": len(data), "warnings": data})
+    
+@api_view(["GET"])
+def status(request):
+    return Response({"status": "ok"})
+
+  
